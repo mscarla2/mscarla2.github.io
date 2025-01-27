176b6146687a8f2161e1fc6d7a2dbbf036563bf7
Marco Scarlata <mscarla2@u.rochester.edu>
2025-01-26 17:13:55 -0800

    [Nav] Progmatize navigation
    Updating nav to be more programmatic.

--- javascript/main.js
+++
@@ -1,9 +0,0 @@
-const home_button = document.getElementById("home");
-const r_button = document.getElementById("resume");
-const ln_button = document.getElementById("linkedin");
-const _button = document.getElementById("github");
-
-
-r_button.onclick = function resume_onClick(){
-    r_button.classList.add("is-active");
-}
--- javascript/nav.js
+++ javascript/nav.js
@@ -1,13 +1,20 @@
-const sidebar = document.getElementById('sidebar');
-const rest = document.getElementById('restofpage');
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("nav.html")
+      .then((response) => response.text())
+      .then((html) => {
+          // Insert the navbar into the page
+          document.getElementById("navbar").innerHTML = html;
 
-sidebar.addEventListener('mouseenter', function() {
-  this.classList.add('expand');
-  rest.classList.add('retract');
+          // Highlight the active link
+          const currentPage = window.location.pathname;
+          const navLinks = document.querySelectorAll("#navbar li");
+          navLinks.forEach((link) => {
+            console.log(currentPage.split("/")[2], link.getAttribute("id"))
+              if (link.getAttribute("id") + ".html" === currentPage.split("/")[2]) {
+                console.log("Here");
+                  link.classList.add("is-active");
+              }
+          });
+      })
+      .catch((error) => console.error("Error loading the navbar:", error));
 });
-
-sidebar.addEventListener('mouseleave', function() {
-  this.classList.remove('expand');
-  rest.classList.remove('retract');
-});
-
