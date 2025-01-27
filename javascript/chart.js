43debc72ed476a2f0854c8d1bb459156055f1357
Marco Scarlata <mscarla2@u.rochester.edu>
2025-01-26 23:17:36 -0800

    [Resume] Change Skills --> Learning Overview
    Change the Skills Overview to Learning Overview to align
    more with my resume. It's also just better visually.

--- javascript/chart.js
+++ javascript/chart.js
@@ -2,10 +2,10 @@ const ctx = document.getElementById('skillsChart').getContext('2d');
 const skillsChart = new Chart(ctx, {
   type: 'radar',
   data: {
-    labels: ['Python', 'C++', 'Java', 'JavaScript', 'Physique'],
+    labels: ['Distributed Systems', 'TensorflowLite', 'TinyML', 'Algo Trading', 'Embedded Dev C++', 'System Design', 'Korean'],
     datasets: [{
-      label: 'Skill Proficiency',
-      data: [10, 10, 5, 4, 9],
+      label: 'Learning Focus',
+      data: [7, 9,10,6,8,7,3],
       backgroundColor: 'rgba(54, 162, 235, 0.2)',
       borderColor: 'rgba(54, 162, 235, 1)',
       borderWidth: 1,
