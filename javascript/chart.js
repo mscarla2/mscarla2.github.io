8789c06b0a76514f550c95b6210c4ba534231ab8
Marco Scarlata <mscarla2@u.rochester.edu>
2025-01-26 23:12:09 -0800

    [Resume] Update Resume.
    Update resume with more up to date content.

--- javascript/chart.js
+++ javascript/chart.js
@@ -2,10 +2,10 @@ const ctx = document.getElementById('skillsChart').getContext('2d');
 const skillsChart = new Chart(ctx, {
   type: 'radar',
   data: {
-    labels: ['Python', 'C++', 'Java', 'JavaScript'],
+    labels: ['Python', 'C++', 'Java', 'JavaScript', 'Physique'],
     datasets: [{
       label: 'Skill Proficiency',
-      data: [10, 10, 5, 4],
+      data: [10, 10, 5, 4, 9],
       backgroundColor: 'rgba(54, 162, 235, 0.2)',
       borderColor: 'rgba(54, 162, 235, 1)',
       borderWidth: 1,
