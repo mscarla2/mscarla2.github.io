d2b9d941537436c44084412c81d286291646f83b
Marco Scarlata <mscarla2@u.rochester.edu>
2025-10-24 22:39:21 -0700

    [Resume] Updating resume page

--- javascript/chart.js
+++ javascript/chart.js
@@ -2,10 +2,10 @@ const ctx = document.getElementById('skillsChart').getContext('2d');
 const skillsChart = new Chart(ctx, {
   type: 'radar',
   data: {
-    labels: ['Distributed Systems', 'TensorflowLite', 'TinyML', 'Algo Trading', 'Embedded Dev C++', 'System Design', 'Korean'],
+    labels: ['Operating Systems', 'C++', 'TensorflowLite', 'TinyML', 'Algo Trading', 'System Design', 'Korean'],
     datasets: [{
       label: 'Learning Focus',
-      data: [7, 9,10,6,8,7,3],
+      data: [9, 6,6,6,9,7,3],
       backgroundColor: 'rgba(54, 162, 235, 0.2)',
       borderColor: 'rgba(54, 162, 235, 1)',
       borderWidth: 1,
